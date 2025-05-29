c38d0bfee7e4119b7303d55557c7bc4d8894ad75
sonneko27 <sonneko27@gmail.com>
2025-05-29 11:08:11 +0000

    add metatag and remove font-setting

---
+++ src/lib/const.ts
@@ -0,0 +1,16 @@
+// 定数
+
+// TODO: 上の人に確認し訂正する
+
+// メタデータ（headタグ内の内容）
+export const METADATA_SITE_URL = "https://sf25-site.******.com/";
+export const METADATA_SITE_NAME = "sf25-site";
+export const METADATA_TITLE = "sf25-site";
+export const METADATA_META_DESCRIPTION = "This is sf25 official website.";
+export const METADATA_OGP_IMAGES_PATH = "https://sf25-site.******.com/ogp_image.jpeg";
+export const METADATA_FAVICON_PATH_16 = "https://sf25-site.******.com/faivicon/x16.jpeg";
+export const METADATA_FAVICON_PATH_32 = "https://sf25-site.******.com/faivicon/x32.jpeg";
+export const METADATA_FAVICON_PATH_48 = "https://sf25-site.******.com/faivicon/x48.jpeg";
+export const METADATA_FAVICON_PATH_62 = "https://sf25-site.******.com/faivicon/x62.jpeg";
+export const METADATA_FAVICON_PATH_APPLE_APP = "https://sf25-site.******.com/faivicon/apple.png";
+
