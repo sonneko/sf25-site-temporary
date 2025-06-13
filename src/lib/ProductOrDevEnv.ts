43f32b1b5fac85bd4d6d17f0b98cd695feeb8066
sonneko27 <sonneko27@gmail.com>
2025-06-13 14:08:27 +0000

    fix improve type check logic of ProductOrDevEnv.init

--- src/lib/ProductOrDevEnv.ts
+++ src/lib/ProductOrDevEnv.ts
@@ -6,18 +6,15 @@ export default class ProductOrDevEnv {
 
     private static init() {
         if (!this.isInited) {
-            try {
-                const envValue = process.env.IS_PRODUCT_OR_DEV_ENV;
-
-                if (envValue === 'product') {
-                    this.env = 'product';
-                } else if (envValue === 'dev') {
-                    this.env = 'dev';
-                } else {
-                    throw new Error('\nIS_PRODUCT_OR_DEV_ENV is illegal value.');
-                }
-            } catch (e) {
-                throw new Error(`\nIS_PRODUCT_OR_DEV_ENV is not defined.\n\n\n${e}`);
+            const envValue = process.env.IS_PRODUCT_OR_DEV_ENV;
+            if (envValue === 'product') {
+                this.env = 'product';
+            } else if (envValue === 'dev') {
+                this.env = 'dev';
+            } else if (envValue === undefined) {
+                throw new Error('\nIS_PRODUCT_OR_DEV_ENV is not configure.');
+            } else {
+                throw new Error(`\nIS_PRODUCT_OR_DEV_ENV is invalid. ${envValue}`);
             }
             this.isInited = true;
         }
